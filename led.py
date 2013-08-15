db12929dce3c7febe091d5a225fb1cbcfc97672a
lxc-xx <lixuanchong@foxmail.com>
2013-08-15 18:56:57 -0400

    Update led.py

---
+++ led.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import os
+import sys
+import getopt
+
+def main(argv):
+    help_output = "Usage: led.py [-f] <file> <code>"
+    err_output = help_output
+    try:
+        opts, args = getopt.getopt(argv, "f", ["help"])
+    except getopt.GetoptError as err:
+        print err
+        sys.exit(1)
+
+    if len(args) != 2:
+        print args
+        print err_output
+        sys.exit(1)
+    
+    path = args[0]
+    code = args[1]
+    ifile = open(path, 'r')
+    
+
+    is_filter = False
+    for o, a in opts:
+        if o == '-f':
+            is_filter = True
+        elif o == '--help':
+            print help_output
+            sys.exit(0)
+
+    for line in ifile.readlines():
+        result = eval_code(code, line)
+        if is_filter:
+            if result:
+                print str(line.rstrip('\n'))
+        else: 
+            print str(result)
+
+    ifile.close()
+
+def eval_code(code, line):
+    return eval(code, {'line':line.rstrip('\n'), 'path':os.path})
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
