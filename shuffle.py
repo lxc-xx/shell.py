4ddda1303059d1fab0b23fabf5b1ae3754a5c933
lxc-xx <lixuanchong@foxmail.com>
2013-09-04 13:51:33 -0400

    Update shuffle.py

---
+++ shuffle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import sys
+import random
+
+def main(argv):
+    file_name = argv[0]
+
+    f = open(file_name, 'r')
+    lines = f.readlines()
+    random.shuffle(lines)
+
+    for line in lines:
+        print line
+
+    return 0
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print "Usage: shuffle.py file"
+        sys.exit(1)
+
+    main(sys.argv[1:])
